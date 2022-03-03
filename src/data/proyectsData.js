23e30bdc7d81019ada5804344518ec96b596bfea
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-03-02 23:28:37 -0300

    typo correction in the proyect info

--- src/data/proyectsData.js
+++ src/data/proyectsData.js
@@ -5,7 +5,7 @@ const proyects = [
     img: './img/proyects/detras-del-armario.png',
     text: [
       'This is a React app made in colaboration with the argentinian writer Sofía Contreras Canard.',
-      'The app was selected one of the 40 finalist of over 4500 articipants in the 2020 Digital Stories Contest by Itaú Fundation. It has also been declared a initiative of cultural interest by the city council of Anisacate in 2021.',
+      'The app was selected one of the 40 finalist of over 4500 participants in the 2020 Digital Stories Contest by Itaú Fundation. It has also been declared a initiative of cultural interest by the city council of Anisacate in 2021.',
       'The app recreates a story writed by Sofía using data provided by the user in an interactive way.',
     ],
     links: {
