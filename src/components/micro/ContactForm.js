a55ae1751c004416a740a32af312d02ce770fc18
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-03-03 22:50:17 -0300

    links animations

--- src/components/micro/ContactForm.js
+++ src/components/micro/ContactForm.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const ContactForm = () => {
   return (
-  <form className='text-center'>
-   <div className="mb-3">
-    <label for="name" className="form-label">Name</label>
-    <input type="text" className="form-control" id="name" aria-describedby="emailHelp" />
-   </div>
-   <div className="mb-3">
-    <label for="email" className="form-label">Email</label>
-    <input type="email" className="form-control" id="email" />
-   </div>
-   <div className="mb-3">
-    <label for="msg" className="form-label">Message</label>
-    <textarea className="form-control" id="msg" rows="3"></textarea>
-   </div>
-   <button type="submit" className="btn btn-dark">Submit</button>
-  </form>
-  )
-}
+    <form className='text-center'>
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          id='name'
+          aria-describedby='emailHelp'
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input type='email' className='form-control' id='email' />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='msg' className='form-label'>
+          Message
+        </label>
+        <textarea className='form-control' id='msg' rows='3'></textarea>
+      </div>
+      <button type='submit' className='btn btn-dark'>
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
